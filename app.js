a1a2916580f182b33b0656a1bb0a22b8507dd1f4
derickongeri <derickongeri@gmail.com>
2023-02-21 11:12:41 +0300

    modified app.js

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 // require packages used in the project
-const express = require('express')
-const cors = require('cors')
-var bodyParser = require('body-parser')
-const ee = require('@google/earthengine');
-const privateKey = require('./agristats-378216-b464955961eb.json');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+var bodyParser = require("body-parser");
+const ee = require("@google/earthengine");
+const privateKey = require("./agristats-378216-b464955961eb.json");
+const app = express();
 
-const port = 3000
+const port = 3000;
 
-const {clipRaster, clipRasterCustomGeometry} = require("./gsnodeclient");
+const { clipRaster, clipRasterCustomGeometry } = require("./gsnodeclient");
 
 // const zonalStats = require('./geoserverWPS')
 
-app.use(cors())
+app.use(cors());
 
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use('/api/rasters/lulc/crop/shape',clipRaster);
+app.use("/api/rasters/lulc/crop/shape", clipRaster);
 
-app.post('/api/rasters/lulc/cropcustom', [jsonParser,clipRasterCustomGeometry] , (req, res) => {
- 
-})
+app.post(
+  "/api/rasters/lulc/cropcustom",
+  [jsonParser, clipRasterCustomGeometry],
+  (req, res) => {}
+);
 
 // app.use('/api/rasters/stats', zonalStats)
 
@@ -31,38 +33,82 @@ app.post('/api/rasters/lulc/cropcustom', [jsonParser,clipRasterCustomGeometry] ,
 // });
 
 // Define endpoint at /mapid.
-app.get('/mapid', (_, response) => {
-  const srtm = ee.Image('CGIAR/SRTM90_V4');
-  const slope = ee.Terrain.slope(srtm);
-  slope.getMap({min: 0, max: 60}, ({mapid}) => response.send(mapid));
-  console.log(response)
+app.get("/mapid", (_, response) => {
+  ee.reset()
+
+  var countries = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017");
+
+  var country = countries.filter(ee.Filter.eq("country_na", "Kenya"));
+
+  var image = ee.Image("UMD/hansen/global_forest_change_2021_v1_9");
+
+  //image = image.clip(country);
+
+  // Define the visualization parameters
+  var visParams = {
+    bands: ["lossyear"],
+    min: 0,
+    max: 21,
+    palette: ["yellow", "red"],
+  };
+
+  // Get the map ID for the image
+  var mapId = image.getMap(visParams);
+
+  // Define the tile coordinates and zoom level
+  var x = 525;
+  var y = 345;
+  var z = 10;
+
+  // Get the URL for the tile using the map ID
+  var url = ee.data.getTileUrl({
+    mapid: mapId.mapid,
+    token: mapId.token,
+    x: x,
+    y: y,
+    z: z,
+  });
+
+  console.log(url);
+
+  // Construct WMS URL
+
+  image.getMap(visParams, ({ mapid }) => {
+    //const wmsUrl = `https://earthengine.googleapis.com/map/${mapid}/wms?`;
+    response.send(mapid);
+    console.log(mapid);
+  });
+
 });
 
-
-console.log('Authenticating Earth Engine API using private key...');
+console.log("Authenticating Earth Engine API using private key...");
 ee.data.authenticateViaPrivateKey(
-    privateKey,
-    () => {
-      console.log('Authentication successful.');
-      ee.initialize(
-          null, null,
-          () => {
-            console.log('Earth Engine client library initialized.');
-            //app.listen(port);
-            console.log(`Listening on port ${port}`);
-          },
-          (err) => {
-            console.log(err);
-            console.log(
-                `Please make sure you have created a service account and have been approved.
-Visit https://developers.google.com/earth-engine/service_account#how-do-i-create-a-service-account to learn more.`);
-          });
-    },
-    (err) => {
-      console.log(err);
-    });
+  privateKey,
+  () => {
+    console.log("Authentication successful.");
+    ee.initialize(
+      null,
+      null,
+      () => {
+        console.log("Earth Engine client library initialized.");
+        //app.listen(port);
+        console.log(`Listening on port ${port}`);
+      },
+      (err) => {
+        console.log(err);
+        console.log(
+          `Please make sure you have created a service account and have been approved.
+Visit https://developers.google.com/earth-engine/service_account#how-do-i-create-a-service-account to learn more.`
+        );
+      }
+    );
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
 // start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+  console.log(`Express is listening on localhost:${port}`);
 });
